942f59b18df73470be0c8c14300999b4b11245ab
Mark Brazeau <kitsapbackflow@gmail.com>
2019-12-31 19:59:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,7 +41,7 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-PORTION_1 = {
+Portion_1 = {
   label: "Kingdom",
   sub_category: {
     label: "Phylum",
@@ -50,5 +50,5 @@ PORTION_1 = {
     }
   }
 }
-PORTION_1
+
 end
